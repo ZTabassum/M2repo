49802a4133d5af44d118037d187f4dbeda829d33
vylos <jma93@gatech.edu>
2017-01-31 12:50:44 -0500

    changed antlabmain from person3 to jeffrey ma

--- M2/M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -41,8 +41,6 @@ import edu.gatech.oad.antlab.pkg2.*;
         ant21 = new AntLab21();
         ant22 = new AntLab22();
         ant31 = new AntLab31();
-        
-        
     }
     
     /**
@@ -65,8 +63,8 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  toPrint += p2.toString("jdavis410");
 		  //Person3 replace P3 with your name
 		  //and gburdell3 with your gt id
-		  Person3 p3 = new Person3("P3");
-		  toPrint += p3.toString("gburdell3");
+		  Person3 p3 = new Person3("Jeffrey Ma");
+		  toPrint += p3.toString("jma93");
                   //Person4 replace P4 with your name
                   //and gburdell4 with your gt id
                   Person4 p4 = new Person4("Michelle");
